becd273c17cb17485660e86143a68cd9ac32b3d2
jssmith79 <i.am.hef@gmail.com>
2023-02-08 20:28:02 -0800

    trying to get the api to work

--- public/index.js
+++ public/index.js
@@ -3,7 +3,20 @@ async function main() {
     const timeChartCanvas = document.querySelector('#time-chart');
     const highestPriceChartCanvas = document.querySelector('#highest-price-chart');
     const averagePriceChartCanvas = document.querySelector('#average-price-chart');
+ 
 
+    const API_KEY = '6a29e68398f84ab68e742e61032201ad';
+
+    async function main() {
+    try {
+        const response = await fetch(`https://api.twelvedata.com/time_series?symbol=AAPL&interval=1min&apikey=${API_KEY}`);
+        const data = await response.json();
+        console.log(data);
+        } catch (error) {
+        console.error(error);
+        }
+    }
+   
 }
 
-main()
+main();
