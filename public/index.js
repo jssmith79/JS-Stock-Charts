acbd23b3eee9db3db57ebfa408321b3191a13e81
jssmith79 <i.am.hef@gmail.com>
2023-02-14 17:21:21 -0800

    update with changes from Hadeel

--- public/index.js
+++ public/index.js
@@ -4,30 +4,19 @@ async function main() {
     const highestPriceChartCanvas = document.querySelector('#highest-price-chart');
     const averagePriceChartCanvas = document.querySelector('#average-price-chart');
    
-    const { GME, MSFT, DIS, BNTX } = mockData;
-    const stocks = [GME, MSFT, DIS, BNTX];
-    
-
 
     const API_KEY = '6a29e68398f84ab68e742e61032201ad';
 
 
-    try {
-        const response = await fetch(`https://api.twelvedata.com/time_series?symbol=GME,MSFT,DIS,BNTX&interval=1day&apikey=${API_KEY}`);
-        const data = await response.json();
-        console.log(data);
-        } catch (error) {
-        console.error(error);
-        }
-    }
+    const response = await fetch(`https://api.twelvedata.com/time_series?symbol=GME,MSFT,DIS,BNTX&interval=1day&apikey=${API_KEY}`);
+    const result = await response.json();
+    console.log(result);
+
    
-
+    const { GME, MSFT, DIS, BNTX} = result;
+    const stocks = [GME, MSFT, DIS, BNTX];
     
-    let GME = result.GME
-    let MSFT = result.MSFT
-    let DIS = result.DIS
-    let BTNX = result.BTNX  
-
+    
     
     // Bonus Note: 
     // Another way to write the above lines would to refactor it as:
@@ -42,23 +31,9 @@ async function main() {
             datasets: [{
                 label: '# of Votes',
                 data: [12, 19, 3, 5, 2, 3],
-                backgroundColor:  'rgba(255, 99, 132, 0.2)',
+                backgroundColor: 'rgba(255, 99, 132, 0.2)',
                 borderColor: 'rgba(255, 99, 132, 1)'
             }]
         }
     });
-    new Chart(timeChartCanvas.getContext('2d'), {
-        type: 'line',
-        data: {
-            labels: stocks[0].values.map(value => value.datetime),
-            datasets: stocks.map( stock => ({
-                label: stock.meta.symbol,
-                data: stock.values.map(value => parseFloat(value.high)),
-                backgroundColor:  getColor(stock.meta.symbol),
-                borderColor: getColor(stock.meta.symbol),
-            }))
-        }
-    });
-        
-
-main();
+}        
